467e4bea4297545448620e317e79d5f8c878fb56
Khelil Sator <khelil.sator.git@gmail.com>
2019-05-26 16:30:57 +0200

    Create accuracy_of_SVC_fitted_with_iris_data_set.py

---
+++ accuracy_of_SVC_fitted_with_iris_data_set.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+iris=load_iris()
+X = iris.data
+Y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.5)
+clf = SVC(kernel='linear')
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+y_pred
+y_test
+accuracy_score(y_test,y_pred)
+print ("Accuracy of SVC fitted with iris data set:",accuracy_score(y_test,y_pred))
